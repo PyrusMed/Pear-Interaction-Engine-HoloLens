3f489c2c8d128945276598640408a5171a7e3a2f
drryanjames <rcjames1004@gmail.com>
2017-09-05 00:20:16 -0700

    Add voice commands from inspector

---
+++ Scripts/Interactions/VoiceCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Pear.InteractionEngine.Interactions
+{
+	/// <summary>
+	/// Maps a voice command to an event function
+	/// </summary>
+	public class VoiceCommand : MonoBehaviour
+	{
+		[Tooltip("Command that fires voice event")]
+		public string Command;
+
+		[Tooltip("Event(s) fired when voice command called")]
+		public UnityEvent Events;
+
+		private void Start()
+		{
+			// When the user says the command execute the action(s)
+			VoiceCommandManager.Instance.ListenForCommand(Command, (ka) => Events.Invoke());
+		}
+	}
+}
